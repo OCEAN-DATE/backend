494b1f8789f3b16e26aac90bd969330999bba3b3
EunLee <leeeun331@gmail.com>
2026-01-09 08:11:03 +0900

    fix: swagger 설정 수정

--- src/main/java/com/oceandate/backend/global/config/SecurityConfig.java
+++ src/main/java/com/oceandate/backend/global/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.oceandate.backend.global.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import com.oceandate.backend.domain.user.service.CustomOAuth2UserService;
-import com.oceandate.backend.domain.user.service.CustomOidcUserService;
 import com.oceandate.backend.global.oauth2.handler.OAuth2AuthenticationFailureHandler;
 import com.oceandate.backend.global.oauth2.handler.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
@@ -60,8 +58,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**").permitAll()
-//                        .anyRequest().authenticated()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
+//                        .anyRequest().permitAll()
                 );
 
         http.sessionManagement(session ->
