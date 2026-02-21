d9a6c8d72495e6d705edccd8197cbeaa855e2fd8
EunLee <leeeun331@gmail.com>
2026-02-21 20:11:00 +0900

    remove: toss 관련 클래스 삭제

--- src/main/java/com/oceandate/backend/domain/payment/dto/PaymentCancelRequest.java
+++ src/main/java/com/oceandate/backend/domain/payment/dto/PaymentCancelRequest.java
@@ -1,11 +1,8 @@
 package com.oceandate.backend.domain.payment.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.oceandate.backend.domain.payment.enums.BankCode;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
