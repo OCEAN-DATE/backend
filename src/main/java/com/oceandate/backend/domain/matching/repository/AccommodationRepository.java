4b7a8b4275d739843fef5ae8e0866269035a3fd6
EunLee <leeeun331@gmail.com>
2026-02-24 11:27:32 +0900

    fix: 실행 오류 수정

--- src/main/java/com/oceandate/backend/domain/matching/repository/AccommodationRepository.java
+++ src/main/java/com/oceandate/backend/domain/matching/repository/AccommodationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
-    List<Accommodation> findByEvent(TravelEvent event);
-
-    void deleteByEvent(TravelEvent event);
+//    List<Accommodation> findByEvent(TravelEvent event);
+//
+//    void deleteByEvent(TravelEvent event);
 }
